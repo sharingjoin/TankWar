3647458f591b169ff0c92d259453d1640b0b0caf
shengtaoyue <2285484343@qq.com>
2018-01-19 14:30:38 +0800

    首次提交日志分析软件！

---
+++ 日志分析软件/MainModel.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Linq;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace RegexLesson02
+{   //MainWindow.xaml写要用到的方法，MainModel.cs實現MainWindow.xaml所需要的方法，MainWindow.xaml.cs調用MainWindow.xaml中說明的方法，
+    //Executed,CanExecute后面的方法,比如 Executed="OnLoad_Executed" CanExecute="OnLoad_CanExecute"
+    class MainModel : INotifyPropertyChanged
+    {
+        public void Load(string aFileName)
+        {
+            string[] aLines = File.ReadAllLines(aFileName);
+            SourceTexts = aLines;
+        }
+        public void LoadRegularFilter(string bFilename)
+        {
+            string[] bLines = File.ReadAllLines(bFilename);
+            // _CheckStrings = new string[10];
+            int i = 0;
+            foreach (string bline in bLines)
+            {
+                _CheckStrings[i++] = bline;
+            }
+        }
+
+        public void DoFilter()
+        {
+            if (string.IsNullOrEmpty(Pattern))
+            {
+                FilteredTexts = new List<string>(SourceTexts);
+            }
+            else
+            {
+                Regex aRegex = new Regex(Pattern);
+                List<string> aLines = new List<string>();
+                foreach (string aLine in SourceTexts)
+                {
+                    if (aRegex.IsMatch(aLine))
+                        aLines.Add(aLine);
+                }
+                FilteredTexts = aLines;
+            }
+        }
+
+        public string[] SourceTexts
+        {
+            get { return _SourceTexts; }
+            set
+            {
+                if (_SourceTexts == value) return;
+                _SourceTexts = value;
+                DoFilter();
+                OnPropertyChanged(nameof(SourceTexts));
+            }
+        }
+        private string[] _SourceTexts;
+
+        public List<string> FilteredTexts { get { return _FilterdTexts; } private set { if (_FilterdTexts == value) return; _FilterdTexts = value; OnPropertyChanged(nameof(FilteredTexts)); } }
+        private List<string> _FilterdTexts;
+
+        public string Pattern { get { return _Pattern; } set { if (_Pattern == value) return; _Pattern = value; OnPropertyChanged(nameof(Pattern)); } }
+        private string _Pattern;
+        //下拉框
+        public string[] CheckStrings { get { return _CheckStrings; } set { if (_CheckStrings == value) return; _CheckStrings = value; OnPropertyChanged(nameof(CheckStrings)); } }
+        private static string[] _CheckStrings = new string[10];
+
+        private void OnPropertyChanged(string aPropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(aPropertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        const string ConnectionString = @"Data Source=(localdb)\Projects;Initial Catalog=DataCeShi;Integrated Security=True; Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;";
+        DataDataContext DataContext=new DataDataContext(ConnectionString);
+        public void Create_DataBase()
+        {
+            try
+            {
+                // 连接数据库引擎
+                using (DataDataContext aDataContext1 = new DataDataContext(ConnectionString))
+                {
+                    if (!aDataContext1.DatabaseExists())
+                    {
+                        aDataContext1.CreateDatabase();
+                        MessageBox.Show("数据库已经创建！");
+                    }
+                    else
+                    {
+                       //MessageBox.Show("数据库已经存在！");
+                    }
+                }
+                DataDataContext aDataContext = new DataDataContext(ConnectionString);
+                DataContext = new DataDataContext(ConnectionString);
+                aDataContext.SubmitChanges();
+            }
+            catch (Exception msg)
+            {
+                MessageBox.Show(msg.Message);
+            }
+
+        }
+
+        private static int index = 0;
+        public void Load_Sql()
+        {
+            DataDataContext aDataContext = new DataDataContext(ConnectionString);
+            Contacts = aDataContext.DataCeShi;
+            foreach (object i in Contacts)
+            {
+                DataCeShi d = (DataCeShi)i;
+                CheckStrings[index++] = d.Regular;
+            }
+        }
+
+        public void Save_DataBase()
+        {
+            if (Pattern != null)
+            {
+                Create_DataBase();
+                DataCeShi aNewContact = new DataCeShi {Regular=Pattern };
+                DataDataContext aDataContext = new DataDataContext(ConnectionString);
+                aDataContext.DataCeShi.InsertOnSubmit(aNewContact);
+                aDataContext.SubmitChanges();
+                Contacts = aDataContext.DataCeShi;
+                foreach (object i in Contacts)
+                {
+                    DataCeShi d = (DataCeShi)i;
+                    CheckStrings[index++] = d.Regular;
+                }
+            }
+            Update();
+        }
+        public void Update()
+        {
+            DataDataContext aDataContext = new DataDataContext(ConnectionString);
+            aDataContext.SubmitChanges();
+        }
+
+        private Table<DataCeShi> _contacts;
+
+        public Table<DataCeShi> Contacts
+        {
+            get
+            {
+                DataDataContext aDataContext = new DataDataContext(ConnectionString);
+                return aDataContext.DataCeShi;
+            }
+            set
+            {
+                if (_contacts == value)
+                    return;
+                _contacts = DataContext.DataCeShi;
+                OnPropertyChanged(nameof(Contacts));
+            }
+        }
+
+    }
+}
---
+++ 日志分析软件/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace RegexLesson02
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            _Model = new MainModel();
+            this.DataContext = _Model;
+        }
+        private MainModel _Model;
+
+        private void OnLoad_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog aDlg = new OpenFileDialog();
+            aDlg.Filter = @"日誌文件|*.log";//@忽略轉義符.$呢？
+            if (aDlg.ShowDialog() != true) return;
+            try
+            {
+                _Model.Load(aDlg.FileName);//調用Load()方法，該方法已經在MainModel.cs中實現。
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void OnLoad_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void OnStartFilter_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+           // MessageBox.Show(_Model.Now_CheckStrings);
+
+            try
+            {
+                _Model.DoFilter();//調用DoFilter()方法，該方法已經在MainModel.cs中實現。
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void OnStartFilter_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void OnLoadRegularFilter_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            //_Model.add_strign();
+            //OpenFileDialog bDialog = new OpenFileDialog();
+            //bDialog.Filter = "正则表达式文件|*.txt;*docx";
+            //if (bDialog.ShowDialog() != true) return;
+            //try
+            //{
+            //    _Model.LoadRegularFilter(bDialog.FileName);
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
+            _Model.Load_Sql();
+        }
+
+        private void OnLoadRegularFilter_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void OnSave_To_Data_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            _Model.Create_DataBase();
+            _Model.Save_DataBase();
+        }
+
+        private void OnSave_To_Data_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+    }
+}
